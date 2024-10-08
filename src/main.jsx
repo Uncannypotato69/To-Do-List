c47b0b9f78987bd8c62c78ca533d8b13ec92c339
UncannyPotato69 <get.it.together.beetch@gmail.com>
2024-10-08 19:49:32 +0530

    added sun icon

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
+import Theme from "./components/header/theme"
 
 
 export default function Main(){
   return(
     <div className="app container section">
+    <Theme/>
     <h1>To-Do List</h1>
   </div>
   )
