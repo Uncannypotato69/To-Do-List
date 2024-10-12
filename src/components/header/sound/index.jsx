37d2be968e21fdaecba4f0faa054441aa326c530
UncannyPotato69 <get.it.together.beetch@gmail.com>
2024-10-12 10:05:15 +0530

    changed made

--- src/components/header/sound/index.jsx
+++ src/components/header/sound/index.jsx
@@ -15,12 +15,12 @@ export default function Sound(){
             console.log('Sound checkbox has been clicked', )
             setIsActive(!isActive)
             }}/>
-        <Boop x={0} y={0} rotation={0} scale={1.2} cls={sound}>
+        <Boop x={-5} y={0} rotation={-5} scale={1} cls={sound}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className={`${classes.icon} ${isActive ? classes.fade : ''}`}>
   <path strokeLinecap="round" strokeLinejoin="round" d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
         </svg>
         </Boop>  
-        <Boop x={0} y={0} rotation={0} scale={1.2} cls={silence}>
+        <Boop x={-5} y={0} rotation={-5} scale={1} cls={silence}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className={`${classes.icon} ${!isActive ? classes.fade : ''}`}>
   <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 9.75 19.5 12m0 0 2.25 2.25M19.5 12l2.25-2.25M19.5 12l-2.25 2.25m-10.5-6 4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
         </svg>
--- src/components/header/theme/index.jsx
+++ src/components/header/theme/index.jsx
@@ -1,27 +1,31 @@
 import { useState } from "react"
+import useSound from "use-sound"
 import classes from "./theme.module.css"
 import Boop from "../boop"
+import onSFX from "/audio/on.mp3"
 
 export default function Theme(){
 
     const [isActive, setIsActive] = useState(false)
+
+    const [play] = useSound(onSFX)
 
     const sunClass = isActive ? `${classes.bottom}` : `${classes.top}`;
     const moonClass = isActive ? `${classes.top}` : `${classes.bottom}`;
 
     return(
         <label htmlFor="checkboxTheme" className={`${classes.label}`}>
-            <input type="checkbox" name="" id="checkboxTheme" className={`${classes.checkbox}`} onChange={() => {
+            <input type="checkbox" name="" id="checkboxTheme" className={`${classes.checkbox}`} onClick={play} onChange={() => {
             console.log('Sound checkbox has been clicked', )
             setIsActive(!isActive)
             }}/>
-            <Boop x={0} y={0} rotation={0} scale={1.2} cls={sunClass}>
+            <Boop x={0} y={0} rotation={0} scale={0.8} timing={300} cls={sunClass}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className={`${classes.icon}  ${isActive ? classes.fade : ''}`}>
   <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
         </svg>
             </Boop>
             
-            <Boop x={0} y={0} rotation={0} scale={1.2} cls={moonClass}>
+            <Boop x={0} y={0} rotation={-20} scale={1} timing={200} cls={moonClass}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className={`${classes.icon} ${!isActive ? classes.fade : ''}`}>
   <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
         </svg>
