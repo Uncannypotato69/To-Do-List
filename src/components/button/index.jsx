9df140c0ebc9a40656b40dfc7487fd3f52c51ad1
UncannyPotato69 <get.it.together.beetch@gmail.com>
2024-10-12 11:18:49 +0530

    Header and button added

---
+++ src/components/button/index.jsx
@@ -0,0 +1,14 @@
+import classes from "./button.module.css"
+import Boop from "../header/boop"
+import { animated } from "@react-spring/web"
+
+export default function Button(){
+    return(
+        <button className={`${classes.button}`} >
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className={`${classes.button__svg}`}>
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+            </svg>
+            <p className={`${classes.button__text}`}>Add</p>
+        </button>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import Theme from "./components/header/theme"
 import Sound from "./components/header/sound"
 import Header from "./components/header"
+import Title from "./components/Title"
+import Button from "./components/button"
 
 
 export default function Main(){
   return(
     <div className="app container section">
     <Header />
-    <h1>To-Do List</h1>
+    <Title/>
+    <Button/>
   </div>
   )
 }
